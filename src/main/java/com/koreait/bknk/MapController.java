c6651f342875eaa7fbb3a5a7e0bd7bc72ac8d034
gimdonghun <hg03087@naver.com>
2021-07-16 16:52:35 +0900

    Initial commit

---
+++ src/main/java/com/koreait/bknk/MapController.java
@@ -0,0 +1,15 @@
+package com.koreait.bknk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/map")
+public class MapController {
+
+    @GetMapping("/map")
+    public void map() {
+    }
+
+}
